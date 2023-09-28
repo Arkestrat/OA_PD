0dfc8394c342f8f9d393087bf00569f27d28352d
Arkestrat <115097705+Arkestrat@users.noreply.github.com>
2023-09-28 19:30:31 +0300

    Add files via upload

---
+++ 4/4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+class Programm
+{
+    public static void Main()
+    {
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        if (a != b)
+        {
+            if (a < b)
+            {
+                a = (a + b) / 2;
+                b = (b * (a * 2 - b)) * 2;
+                Console.WriteLine($"x больше {a} и {b}");
+            }
+            else
+            {
+                b = (a + b) / 2;
+                a = (a * (b * 2 - a)) * 2;
+                Console.WriteLine($"x больше {a} и {b}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("y больше");
+        }
+    }
+}
